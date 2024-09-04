49e8e2c269e9654dbe13e99e93593a211d0ded35
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2024-09-03 17:31:19 -0700

    Optimized contracts

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
@@ -13,6 +13,7 @@ import "./IMintNFT.sol";
 error INSUFFICIENT_BALANCE();
 error NO_ALLOWANCE();
 error PAYMENT_FAILED();
+error WITHDRAW_FAILED();
 error INVALID_ARRAY_LENGTH();
 error INVALID_TIER();
 error MAX_MINT_PER_WALLET_EXCEEDED();
@@ -27,6 +28,7 @@ contract Minter is
 {
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     IMintNFT private mintNFT;
     IERC20 private paymentToken;
 
@@ -58,16 +60,15 @@ contract Minter is
     }
 
     /**
-     * @dev Initializes the contract with the given manager, avatar NFT address, and payment token address.
-     * Grants the DEFAULT_ADMIN_ROLE and MANAGER_ROLE to the deployer and the specified manager.
+     * @notice Initializes the contract with the given manager, avatar NFT address, and payment token address.
      * @param _manager The address of the manager.
-     * @param _mintNFTAddress The address of the Avatar NFT contract.
-     * @param _paymentTokenAddress The address of the payment token contract.
+     * @param _mintNFT The address of the Avatar NFT contract.
+     * @param _paymentToken The address of the payment token contract.
      */
     function initialize(
         address _manager,
-        address _mintNFTAddress,
-        address _paymentTokenAddress
+        address _mintNFT,
+        address _paymentToken
     ) public initializer {
         __Pausable_init();
         __AccessControl_init();
@@ -76,16 +77,13 @@ contract Minter is
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MANAGER_ROLE, _manager);
 
-        mintNFT = IMintNFT(_mintNFTAddress);
-        paymentToken = IERC20(_paymentTokenAddress);
+        mintNFT = IMintNFT(_mintNFT);
+        paymentToken = IERC20(_paymentToken);
         maxMintPerWallet = 20;
     }
 
-    // Set functions
-
     /**
-     * @dev Sets a single tier with the given index, price, and ranges.
-     * Can only be called by an account with the MANAGER_ROLE.
+     * @notice Sets a single tier with the given index, price, and ranges.
      * @param _index The index of the tier.
      * @param _price The price of the tier.
      * @param _ranges The ranges of the tier.
@@ -100,9 +98,7 @@ contract Minter is
     }
 
     /**
-     * @dev Sets multiple tiers with the given indexes, prices, and ranges.
-     * Can only be called by an account with the MANAGER_ROLE.
-     * Reverts if the lengths of the input arrays do not match.
+     * @notice Sets multiple tiers with the given indexes, prices, and ranges.
      * @param _indexes The indexes of the tiers.
      * @param _prices The prices of the tiers.
      * @param _ranges The ranges of the tiers.
@@ -124,8 +120,7 @@ contract Minter is
     }
 
     /**
-     * @dev Sets the maximum number of mints allowed per wallet.
-     * Can only be called by an account with the MANAGER_ROLE.
+     * @notice Sets the maximum number of mints allowed per wallet.
      * @param _maxMintPerWallet The maximum number of mints per wallet.
      */
     function setMaxMintPerWallet(
@@ -135,31 +130,25 @@ contract Minter is
     }
 
     /**
-     * @dev Sets the address of the Avatar NFT contract.
-     * Can only be called by an account with the MANAGER_ROLE.
-     * @param _mintNFTAddress The address of the Avatar NFT contract.
+     * @notice Sets the address of the Avatar NFT contract.
+     * @param _mintNFT The address of the Avatar NFT contract.
      */
-    function setMintNFT(
-        address _mintNFTAddress
-    ) external onlyRole(MANAGER_ROLE) {
-        mintNFT = IMintNFT(_mintNFTAddress);
+    function setMintNFT(address _mintNFT) external onlyRole(MANAGER_ROLE) {
+        mintNFT = IMintNFT(_mintNFT);
     }
 
     /**
-     * @dev Sets the address of the payment token contract.
-     * Can only be called by an account with the MANAGER_ROLE.
-     * @param _paymentTokenAddress The address of the payment token contract.
+     * @notice Sets the address of the payment token contract.
+     * @param _paymentToken The address of the payment token contract.
      */
     function setPaymentToken(
-        address _paymentTokenAddress
+        address _paymentToken
     ) external onlyRole(MANAGER_ROLE) {
-        paymentToken = IERC20(_paymentTokenAddress);
+        paymentToken = IERC20(_paymentToken);
     }
 
     /**
-     * @dev Withdraws tokens from the contract to the specified treasury address.
-     * Can only be called by an account with the MANAGER_ROLE.
-     * Reverts if the contract has zero balance of the specified token.
+     * @notice Withdraws tokens from the contract to the specified treasury address.
      * @param _token The address of the token to withdraw.
      * @param _treasury The address of the treasury to receive the tokens.
      */
@@ -169,16 +158,13 @@ contract Minter is
     ) external virtual onlyRole(DEFAULT_ADMIN_ROLE) nonReentrant {
         uint256 _amount = IERC20(_token).balanceOf(address(this));
         if (_amount == 0) revert ZERO_BALANCE();
-        IERC20(_token).transfer(_treasury, _amount);
+        if (!IERC20(_token).transfer(_treasury, _amount))
+            revert WITHDRAW_FAILED();
         emit FundsWithdrawn(_token, _treasury, _amount);
     }
 
-    // Mint functions
-
     /**
-     * @dev Mints a new token to the specified address.
-     * Requires the caller to have sufficient balance of the payment token.
-     * Reverts if the recipient has reached the maximum mint limit per wallet.
+     * @notice Mints a new token to the specified address.
      * @param _to The address to mint the token to.
      * @param _tokenId The ID of the token to mint.
      */
@@ -203,9 +189,7 @@ contract Minter is
     }
 
     /**
-     * @dev Mints multiple tokens to the specified address.
-     * Requires the caller to have sufficient balance of the payment token.
-     * Reverts if the recipient has reached the maximum mint limit per wallet.
+     * @notice Mints multiple tokens to the specified address.
      * @param _to The address to mint the tokens to.
      * @param _tokenIds The IDs of the tokens to mint.
      */
@@ -230,13 +214,12 @@ contract Minter is
 
         for (uint256 i = 0; i < _tokenIds.length; i++) {
             mintNFT.mint(_to, _tokenIds[i]);
+            emit TokenMinted(_to, _tokenIds[i]);
         }
     }
 
-    // Get functions
-
     /**
-     * @dev Returns the address payment token contract.
+     * @notice Returns the address of the payment token contract.
      * @return The address of the payment token contract.
      */
     function getPaymentToken() external view returns (address) {
@@ -244,7 +227,7 @@ contract Minter is
     }
 
     /**
-     * @dev Returns the address of the Avatar NFT contract.
+     * @notice Returns the address of the Avatar NFT contract.
      * @return The address of the Avatar NFT contract.
      */
     function getMintNFT() external view returns (address) {
@@ -252,7 +235,7 @@ contract Minter is
     }
 
     /**
-     * @dev Returns the tier index for the given token ID.
+     * @notice Returns the tier index for the given token ID.
      * @param _tokenId ID of the token.
      * @return The index of the tier.
      */
@@ -261,7 +244,7 @@ contract Minter is
     }
 
     /**
-     * @dev Returns the price for the given token ID.
+     * @notice Returns the price for the given token ID.
      * @param _tokenId The ID of the token.
      * @return The price of the token.
      */
@@ -270,7 +253,7 @@ contract Minter is
     }
 
     /**
-     * @dev Returns the tier at the specified index.
+     * @notice Returns the tier at the specified index.
      * @param _index The index of the tier.
      * @return The tier at the specified index.
      */
@@ -278,13 +261,11 @@ contract Minter is
         return tiers[_index];
     }
 
-    // Internal functions
-
     /**
-     * @dev Internal function to set a tier with _tokenIdven index, price, and ranges.
+     * @notice Internal function to set a tier with the given index, price, and ranges.
      * @param _index The index of the tier.
-     * @param _price The price of the tier._tokenId
-     * @param _ranges The ranges of the tier._tokenId
+     * @param _price The price of the tier.
+     * @param _ranges The ranges of the tier.
      */
     function _setTier(
         uint8 _index,
@@ -293,19 +274,15 @@ contract Minter is
     ) internal {
         Tier storage tier = tiers[_index];
         tier.price = _price;
-        delete tier.ranges;
-        for (uint256 i = 0; i < _ranges.length; ) {
+        tier.ranges = new uint256[2][](0);
+        for (uint256 i = 0; i < _ranges.length; i++) {
             tier.ranges.push(_ranges[i]);
-            unchecked {
-                i++;
-            }
         }
         emit TierSet(_index, _price, _ranges);
     }
 
     /**
-     * @dev Internal function to get the price for the given token ID.
-     * Reverts if the tier is invalid.
+     * @notice Internal function to get the price for the given token ID.
      * @param _tokenId The ID of the token.
      * @return The price of the token.
      */
@@ -316,51 +293,66 @@ contract Minter is
     }
 
     /**
-     * @dev Internal function to get the tier index for the given token ID.
-     * Reverts if the tier is invalid.
+     * @notice Internal function to get the tier index for the given token ID.
      * @param _tokenId The ID of the token.
      * @return The index of the tier.
      */
     function _getTokenTier(uint256 _tokenId) internal view returns (uint8) {
         for (uint8 i = 0; i < MAX_TIERS; i++) {
             if (tiers[i].price > 0) {
-                for (uint256 j = 0; j < tiers[i].ranges.length; j++) {
-                    if (
-                        _tokenId >= tiers[i].ranges[j][0] &&
-                        _tokenId <= tiers[i].ranges[j][1]
-                    ) {
-                        return i;
-                    }
+                if (_binarySearch(tiers[i].ranges, _tokenId)) {
+                    return i;
                 }
             }
         }
         revert INVALID_TIER();
     }
 
-    // Pause and upgrade functions (Inheritance)
+    /**
+     * @notice Internal function to check if the token ID is within the specified ranges.
+     * @param ranges The ranges to check.
+     * @param tokenId The ID of the token.
+     * @return True if the token ID is within the ranges, false otherwise.
+     */
+    function _binarySearch(
+        uint256[2][] storage ranges,
+        uint256 tokenId
+    ) internal view returns (bool) {
+        uint256 low = 0;
+        uint256 high = ranges.length - 1;
+
+        while (low <= high) {
+            uint256 mid = (low + high) / 2;
+            if (tokenId >= ranges[mid][0] && tokenId <= ranges[mid][1]) {
+                return true;
+            } else if (tokenId < ranges[mid][0]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
 
     /**
-     * @dev Pauses all token transfers.
-     * Can only be called by an account with the MANAGER_ROLE.
+     * @notice Pauses all token transfers.
      */
     function pause() external onlyRole(MANAGER_ROLE) {
         _pause();
     }
 
     /**
-     * @dev Unpauses all token transfers.
-     * Can only be called by an account with the MANAGER_ROLE.
+     * @notice Unpauses all token transfers.
      */
     function unpause() external onlyRole(MANAGER_ROLE) {
         _unpause();
     }
 
     /**
-     * @dev Authorizes an upgrade to a new implementation.
-     * Can only be called by an account with the MANAGER_ROLE.
+     * @notice Authorizes an upgrade to a new implementation.
      * @param newImplementation The address of the new implementation.
      */
     function _authorizeUpgrade(
         address newImplementation
-    ) internal override onlyRole(MANAGER_ROLE) {}
+    ) internal override onlyRole(UPGRADER_ROLE) {}
 }
